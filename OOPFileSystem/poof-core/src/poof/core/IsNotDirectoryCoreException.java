0d7c8495b90582b9f3630bb590686fed104775d6
iluxonchik <iluxon4ik@hotmail.com>
2014-11-12 08:35:02 +0000

    Insignificant fixes.

---
+++ OOPFileSystem/poof-core/src/poof/core/IsNotDirectoryCoreException.java
@@ -0,0 +1,10 @@
+package poof.core;
+
+public class IsNotDirectoryCoreException extends FileSystemManagerException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3707185537969654395L;
+
+}
